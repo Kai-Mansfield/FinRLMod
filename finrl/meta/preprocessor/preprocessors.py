433970cb59a5757f5fd488f5b3972b8cc851415d
Kai <50274436+Kai-Mansfield@users.noreply.github.com>
2023-11-08 06:50:51 +0000

    change drop null axis in clean_data from 1 to 0

--- finrl/meta/preprocessor/preprocessors.py
+++ finrl/meta/preprocessor/preprocessors.py
@@ -118,7 +118,7 @@ class FeatureEngineer:
         df = df.sort_values(["date", "tic"], ignore_index=True)
         df.index = df.date.factorize()[0]
         merged_closes = df.pivot_table(index="date", columns="tic", values="close")
-        merged_closes = merged_closes.dropna(axis=1)
+        merged_closes = merged_closes.dropna(axis=0)
         tics = merged_closes.columns
         df = df[df.tic.isin(tics)]
         # df = data.copy()
